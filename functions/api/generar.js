b1fdb25b3c3a809e9e8b956e3747a20f4652ba50
ibanezvizoso <ibanez.vizoso@gmail.com>
2026-09-15 22:14:13 +0200

    Update generar.js

--- functions/api/generar.js
+++ functions/api/generar.js
@@ -360,7 +360,7 @@ const masterTemplateHtml = `<!DOCTYPE html>
       margin-left: 10px;
     }
     .brand-title img { 
-      height: 56px; 
+      height: 44px; 
       width: auto; 
       display: block; 
     }
